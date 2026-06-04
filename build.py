d5a762b72de7a28eeff059353148d2ac05da6ed7
Nico Verbruggen <mail@nicoverbruggen.be>
2026-06-04 09:25:22 +0200

    Adjusted distance between glyphs

--- build.py
+++ build.py
@@ -63,7 +63,7 @@ with open(os.path.join(ROOT_DIR, "VERSION")) as version_file:
 with open(os.path.join(ROOT_DIR, "COPYRIGHT")) as copyright_file:
     COPYRIGHT_TEXT = copyright_file.read().strip()
 
-DEFAULT_FAMILY = "Libron I"
+DEFAULT_FAMILY = "Libron II"
 
 # (style_suffix, source_sfd, embolden_method)
 # Bold reuses the upright master. The emboldening method per style:
